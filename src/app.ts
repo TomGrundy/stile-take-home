84b01cf6b2c156cb0abf362fce4e8aae17947e0c
Thomas Grundy <me@thomasgrundy.com>
2024-05-22 16:00:58 -0400

    Forgot to commit an explanatory comment in my code. Here it is

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+// This has the effect of only having us accept xml if it's with the header mentioned
+// in the test data, but if the client wants it some other way, tell 'em to put in a 
+// ticket for it.
 xmlparser.regexp = /text\/xml\+markr/i;
 app.use(xmlparser());
 
